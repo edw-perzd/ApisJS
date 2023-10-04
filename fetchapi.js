5aa65969f5ae881e39505fda2071c6c26724b963
edw-perzd <legna246.dav@gmail.com>
2023-10-04 10:12:17 -0600

    03 Fetch API

---
+++ fetchapi.js
@@ -0,0 +1,13 @@
+const url = "http://127.0.0.1:5500/";
+
+console.log('Inicia Petición 1');
+fetch(url + 'respuesta.txt')
+    .then(respuesta => respuesta.text())
+    .then(datos => console.log(datos))
+    .catch(error => console.log(error))
+    
+    console.log('Inicia Petición 2');
+    fetch(url + 'respuesta.json')
+    .then(respuesta => respuesta.json())
+    .then(datos => console.log(datos))
+    .catch(error => console.log(error))
