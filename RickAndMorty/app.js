448da772a3ded85bb643163efa0e3b886227384e
edw-perzd <legna246.dav@gmail.com>
2023-10-11 10:29:46 -0600

    06 Rick and Morty Parte 3

--- RickAndMorty/app.js
+++ RickAndMorty/app.js
@@ -1,46 +1,31 @@
+import { createCard, createCardModal, createPag } from './ui.js';
+
 const URL = 'https://rickandmortyapi.com/api';
 const containerChar = document.querySelector('.characters');
 const pagination = document.querySelector('.pagination');
+const modalTitle = document.querySelector('.modal-title');
+const modalBody = document.querySelector('.modal-body');
+const fetchApi = (url) => fetch(url).then(response => response.json());
 
-function createPag(){
-  let buttons = ``;
-  for(let i = 1; i<= 42; i++){
-    buttons += `
-    <li class="page-item">
-      <a class="page-link" href="#" data-id="${ i }">${ i }</a>
-    </li>`;
-  }
-  pagination.innerHTML = buttons;
-}
-
-createPag();
+pagination.innerHTML = createPag();
 
 function getCharacters(page=1){
-  fetch(`${ URL }/character/?page=${ page }`)
-      .then(response => response.json())
+  fetchApi(`${ URL }/character/?page=${ page }`)
       .then(data => {
-          console.log(data.info);
           const characters = data.results;
           showCharaters(characters);
       });
 }
 
-getCharacters();
-
-function createCard(character){
-    const card = document.createElement('div');
-    card.classList.add('card', 'mt-3', 'bg-secondary-subtle');
-    card.style.width = '18rem';
-    const htmlCard = `
-    <img src="${ character.image }" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${ character.name }</h5>
-      <p class="card-text">${ character.status }</p>
-      <p class="card-text">${ character.origin.name }</p>
-      <a href="#" class="btn btn-success" data-id="${ character.id }">Ver más</a>
-    </div>`;
-    card.innerHTML = htmlCard;
-    return card;
+function getCharactesById(id){
+  fetchApi(`${ URL }/character/${ id }`)
+      .then(data => {
+          const character = data;
+          //console.log(character);
+          modalTitle.innerHTML = character.name;
+          modalBody.innerHTML = '';
+          modalBody.appendChild(createCardModal(character));
+      });
 }
 
 function showCharaters(characters){
@@ -53,8 +38,22 @@ function showCharaters(characters){
 function getPag(e){
   e.preventDefault();
   if(e.target.classList.contains('page-link')){
-    id = e.target.getAttribute('data-id');
-    getCharacters(id);
+    const page = e.target.getAttribute('data-id');
+    getCharacters(page);
   }
 }
+
+function getbuttonCard(e){
+  e.preventDefault();
+  if(e.target.classList.contains('btn')){
+    const id = e.target.getAttribute('data-id');
+    getCharactesById(id);
+  }
+}
+
+
+createPag();
+getCharacters();
+
 pagination.addEventListener('click', getPag);
+containerChar.addEventListener('click', getbuttonCard);
